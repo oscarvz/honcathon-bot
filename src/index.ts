cfbce7bcedb3781c10311c935fd2f33ed7a9532c
oscarvz <oscarvz@users.noreply.github.com>
2024-07-04 09:12:13 +0200

    Add FPX & Biome

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { createHonoMiddleware } from "@fiberplane/hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// NOTE - You should pass the `app` to createHonoMiddleware
+//        to be able to tell fpx about your registered routes
+app.use(createHonoMiddleware(app));
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+// With the beta middleware, this fetch call will be logged in the fpx studio UI
+app.get("/fetch", async (c) => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data: unknown = await response.json();
+    return c.json(data);
+  } catch (error) {
+    return c.json({ error: "Failed to fetch data" }, 500);
+  }
+});
+
+export default app;
